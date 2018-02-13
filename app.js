544a1c5063440a5792ce2cb4aaa0b79158604cd9
chintu12345 <akashmallick600@gmail.com>
2018-02-13 00:53:29 -0800

    aaa

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 var myapp = angular.module('myapp',[]);
-console.log(myapp);
+//console.log(myapp);
+
+myapp.controller('MyCtrl',['$scope', 'AppService',
+    function($scope, AppService){
+
+        AppService.getStudents().then(function(result){
+
+            $scope.students = result.data.map(function(item){
+                item.seniority = item.age>10? "Senior" : "Junior";
+                return item;
+            });
+        })
+
+      //$scope.students = AppService.getStudents();
+
+       $scope.onClick = function(students){
+        $scope.selected = students;
+    };
+    
+    
+   
+    }]); //Dependency Injection
---
+++ app.service.js
@@ -0,0 +1,12 @@
+var myapp = angular.module('myapp');
+myapp.service('AppService',['$http',function($http){
+    
+    
+    
+this.getStudents = function(){
+    return $http.get('/students.json');
+    
+}
+    
+
+}]);
